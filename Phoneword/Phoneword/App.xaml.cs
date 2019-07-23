47a2663bc8917351a16a773784a7c75e11b6b723
Railson Rames <rames.cloud@outlook.com.br>
2019-07-23 17:50:03 -0300

    Add project files.

---
+++ Phoneword/Phoneword/App.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Phoneword
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+
+            var button = new Button()
+            {
+                Text = "Click me"
+            };
+
+            var layout = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical
+            };
+
+            layout.Children.Add(new Label { Text = "Enter Name" });
+            layout.Children.Add(new Entry());
+            layout.Children.Add(new Button { Text = "Ok" });
+
+            MainPage.BindingContext = layout;
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
